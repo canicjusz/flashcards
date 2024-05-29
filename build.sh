dedfbace0ea3c0277dea8d723362d7163d0a5a67
canicjusz <janek.bananek371@gmail.com>
2024-05-29 13:41:56 +0200

    clean up the root directory

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 rm -rf application.zip
 rm -rf flashcards.zip
 npm run build
-cp flashcards_56.png flashcards_112.png manifest.webapp dist
+cp assets/flashcards_56.png assets/flashcards_112.png assets/manifest.webapp dist
 cd dist
 zip -q ../application.zip  css/* index.html js/* flashcards_56.png flashcards_112.png manifest.webapp
 cd ..
-zip -q flashcards.zip application.zip metadata.json update.webapp
+zip -qj flashcards.zip application.zip assets/metadata.json assets/update.webapp
+rm -rf application.zip
